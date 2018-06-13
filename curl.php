33e9ce01e5e237c903323af0f5a2e8e459049c71
ᦖєgιт <support@legitsoulja.info>
2018-06-13 03:07:47 -0400

    Update curl.php

--- curl.php
+++ curl.php
@@ -24,71 +24,100 @@ class Curl {
   }
 
   public static function post($data, $options, $callback){
-    if(self::parse(self::array_keys_exists(array('url', 'fields'), $data))) {
-      $curl = curl_init($data['url']);
-      curl_setopt($curl, CURLOPT_POST, 1);
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-      curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query(($data['fields'] == null) ? array() : $data['fields']));
 
-      // additional options
-      if(is_array($options)) {
-        foreach($options as $opt => $v) {
-          curl_setopt($curl, $opt, $v);
-        }
-      }
-
-      $obj = (object) array('response'=>curl_exec($curl), 'info' => curl_getinfo($curl));
-      curl_close($curl);
-
-      if(is_object($callback)) {
-        return $callback($obj->response, $obj->info);
-      }
-
-      return $obj;
-
+    if(!filter_var($data['url'], FILTER_VALIDATE_URL)) {
+      throw new Exception("Invalid URL provided");
     }
+
+    echo ((is_array($data['query']) ? count($data['query']) : 0));
+
+    $curl = curl_init($data['url']);
+    curl_setopt($curl, CURLOPT_POST, ((is_array($data['query']) ? count($data['query']) : 0)));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query((array_key_exists('query', $data) && is_array($data['query'])) ? $data['query']: array()));
+
+    if(array_key_exists('type', $data)) {
+      curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: '.$data['type']));
+    }
+
+    if(is_array($options)) {
+      foreach($options as $opt => $v) {
+        curl_setopt($curl, $opt, $v);
+      }
+    }
+
+    $obj = (object) array('response'=>curl_exec($curl), 'info' => curl_getinfo($curl));
+    curl_close($curl);
+
+    if(is_object($callback)) {
+      return $callback($obj->response, $obj->info);
+    }
+
+    return $obj;
+
   }
 
   public static function get($data, $options, $callback){
-    if(self::parse(self::array_keys_exists(array('url', 'fields'), $data))) {
-      $curl = curl_init();
-      curl_setopt($curl, CURLOPT_URL, $data['url']."?".http_build_query(($data['fields'] == null) ? array() : $data['fields']));
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-      curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
-      curl_setopt($curl, CURLOPT_HTTPGET, 1);
 
-      // additional options
-      if(is_array($options)) {
-        foreach($options as $opt => $v) {
-          curl_setopt($curl, $opt, $v);
-        }
+    if(!filter_var($data['url'], FILTER_VALIDATE_URL)) {
+      throw new Exception("Invalid URL provided");
+    }
+
+    $curl = curl_init($data['url']."?".http_build_query((array_key_exists('query', $data) && is_array($data['query'])) ? $data['query']: array()));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($curl, CURLOPT_HTTPGET, 1);
+
+    if(array_key_exists('type', $data)) {
+      curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: '.$data['type']));
+    }
+
+    if(is_array($options)) {
+      foreach($options as $opt => $v) {
+        curl_setopt($curl, $opt, $v);
       }
+    }
 
-      $obj = (object) array('response'=>curl_exec($curl), 'info' => curl_getinfo($curl));
-      curl_close($curl);
+    $obj = (object) array('response'=>curl_exec($curl), 'info' => curl_getinfo($curl));
+    curl_close($curl);
 
-      if(is_object($callback)) {
-        return $callback($obj->response, $obj->info);
-      }
+    if(is_object($callback)) {
+      return $callback($obj->response, $obj->info);
+    }
 
-      return $obj;
+    return $obj;
 
+
+  }
+
+  public static function test($a = null){
+    switch(strtolower($a)) {
+      case 'post':
+        self::post(
+          array(
+            'url'=>'http://httpbin.org/post', 
+            'query'=>array('type'=>'post', 'data'=>array('post')),
+            'type'=>'application/json'
+          ), null, 
+          function($response, $info){
+            print_r(func_get_args());
+          }
+        );
+        break;
+      case 'get':
+        self::get(
+          array(
+            'url'=>'http://httpbin.org/get', 
+            'query'=>array('type'=>'get', 'data'=>array('get')),
+            'type' => 'application/json'
+          ), null, 
+          function($response, $info){
+            print_r(func_get_args());
+          }
+        );
+      default: return self::test('post');
     }
   }
 
-  public static function test(){
-    self::get(
-      array(
-        'url'=>'http://httpbin.org/response-headers', 
-        'fields'=>array("a"=>"1", "b"=>2)
-      ), null, 
-      function($response, $info){
-
-        print_r(func_get_args());
-      }
-    );
-  }
-
 };
-
