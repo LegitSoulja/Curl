b118b4571177edea788be76a204b667742620ed2
ᦖєgιт <support@legitsoulja.info>
2018-06-13 13:03:27 -0400

    Update curl.php

--- curl.php
+++ curl.php
@@ -2,26 +2,6 @@
 
 class Curl {
 
-  private static function array_keys_exists($needles, &$haystack) {
-    if(!is_array($needles)) return array(0x3);
-    foreach($needles as &$needle) {
-      if(!array_key_exists($needle, $haystack)) 
-        return array(0x0, $needle);
-      if(is_string($haystack[$needle]) && strlen($haystack[$needle]) < 1) 
-        return array(0x1, $needle);
-    }
-    return array(0x2, $needle);
-  }
-
-  private static function parse($v){
-    switch($v[0]) {
-      case 0x0: throw new Exception("[Curl] No key present for `".$v[1]."`");
-      case 0x1: throw new Exception("[Curl] No data present for `".$v[1]."`");
-      case 0x2: return true;
-      case 0x3: throw new Exception("[Curl] array_keys_exists needles must be an array");
-      default: throw new Exception("[Curl] An error has occured with parsing.");
-    }
-  }
 
   public static function post($data, $options, $callback){
 
@@ -78,7 +58,6 @@ class Curl {
     }
 
     return $obj;
-
 
   }
 
